b2c905730d9c37e62b78fd1c96dfff76c73825b1
vladimir latatye <latatye36@gmail.com>
2026-09-08 19:25:23 +0300

    Сделана п1.2.1

---
+++ 121-blink-reg/main.c
@@ -0,0 +1,19 @@
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+const uint LED_PIN = 25;
+
+int main()
+{
+    // весь дальнейший код пишем здесь
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (1)
+    {
+        // код мигания пишем здесь
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(1000);
+    }
+}
