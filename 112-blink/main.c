1b69c0ef12bce11cc103a8740fb3cca113de0e48
vladimir latatye <latatye36@gmail.com>
2026-09-08 19:27:28 +0300

    повтор

--- 112-blink/main.c
+++ 112-blink/main.c
@@ -1,5 +1,7 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "hardware/regs/addressmap.h"
+#include "hardware/regs/sio.h"
 // на проверку
 const uint LED_PIN = 25;
 const uint Empty = 0;
@@ -8,12 +10,15 @@ int main()
     // весь дальнейший код пишем здесь
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+    volatile uint32_t *gpio_out_set = (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_SET_OFFSET);
+    volatile uint32_t *gpio_out_clr = (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_CLR_OFFSET);
+    const uint32_t led_mask = 1u << LED_PIN;
     while (1)
     {
-        // код мигания пишем здесь
-        gpio_put(LED_PIN, 1);
+        // код мигания через регистры
+        *gpio_out_set = led_mask;
         sleep_ms(250);
-        gpio_put(LED_PIN, 0);
+        *gpio_out_clr = led_mask;
         sleep_ms(1000);
     }
 }
